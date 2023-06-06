24554b5c13eeb396aba63dacd8dc08a41b414f36
kimsiyeong <20211117@sungshin.ac.kr>
2023-06-06 19:57:54 +0900

    로그아웃시 로그인요청

--- controllers/store/bouquetController.js
+++ controllers/store/bouquetController.js
@@ -1,11 +1,15 @@
 const bouquetModel = require("../../models/bouquet");
+const checkLogin = require("../../util/checkLogin");
 
 module.exports = {
   addToBouquet: async (req, res) => {
+    checkLogin;
     try {
       // 세션에서 구매자 ID 추출 
       const buyerId = req.session.user.id;
       const sellerId = req.session.sellerId;
+
+      
       //console.log("부케 - 구매자아이디 세션 확인: " + buyerId);
       //console.log("부케 -  판매자아이디 세션 확인: " + sellerId);
     
@@ -33,7 +37,9 @@ module.exports = {
       res.redirect(`/store/${sellerId}`);
     } catch (err) {
       console.error(err);
-      res.redirect('/user/login');
+
+      // user id 없으면 리다이렉트
+      //res.redirect('/user/login');
       //res.status(500).send('Error adding flower to bouquet');
     }
   },
@@ -53,3 +59,53 @@ module.exports = {
     }
   }
 };
+
+// module.exports = {
+//   addToBouquet: async (req, res) => {
+
+
+//     try {
+//       // 사용자 로그인 체크
+//       checkLogin(req, res, async () => {
+//         // 세션에서 구매자 ID 추출 
+//         const buyerId = req.session.user.id;
+//         const sellerId = req.session.sellerId;
+
+//         let bouquetId = req.session.bouquetId; // 세션에 저장된 장바구니 아이디 읽어오기
+
+//         if (!bouquetId) {
+//           // 꽃다발 아이디가 없는 경우, 꽃다발 새로 생성
+//           bouquetId = await bouquetModel.createBouquet(buyerId, sellerId);
+//           req.session.bouquetId = bouquetId; // 생성된 장바구니 아이디 세션에 저장
+//         }
+
+//         console.log("부케아이디확인: " + bouquetId)
+
+//         // 꽃다발에 꽃 추가
+//         const flowerId = req.body.flowerId;
+//         const quantity = req.body.quantity;
+//         await bouquetModel.insertToBouquet(bouquetId, flowerId, quantity);
+
+//         res.redirect(`/store/${sellerId}`);
+//       });
+//     } catch (err) {
+//       console.error(err);
+//       res.status(500).send('Error adding flower to bouquet');
+//     }
+//   },
+//   showBouquet: async (req, res) => {
+//     try {
+//       const bouquetId = req.session.bouquetId;
+//       const sellerId = req.session.sellerId;
+//       const bouquetItems = await bouquetModel.getBouquetItems(bouquetId, sellerId);
+//       console.log(bouquetItems.items);
+//       console.log(bouquetItems.totalPrice);
+
+//       res.render('store/bouquet', { bouquetItems });
+
+//     } catch (err) {
+//       console.error(err);
+//       res.status(500).send('Error displaying cart');
+//     }
+//   }
+// };
