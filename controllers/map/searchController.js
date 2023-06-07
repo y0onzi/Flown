b1077c82409f16b2bf6571d81a5b4fb258cd3964
kimsiyeong <20211117@sungshin.ac.kr>
2023-06-07 17:55:14 +0900

    지역검색 최종수정

--- controllers/map/searchController.js
+++ controllers/map/searchController.js
@@ -1,22 +1,57 @@
 const searchModel = require('../../models/searchModel');
 
+// exports.renderSearchPage = (req, res) => {
+//   const cities = ['서울', '경기도', '부산광역시'];
+//   const districts = {
+//     '서울': ['성북구', '마포구', '관악구'],
+//     '경기도': ['성북구', '용인시', '성남시'],
+//     '부산광역시': ['해운대구', '사하구', '연제구']
+//   };
+//   const neighborhoods = {
+//     '성북구': ['돈암동', '길음동', '보문동'],
+//     '마포구': ['합정동', '망원동', '상암동'],
+//     '송파구': ['잠실2동', '잠실본동', '가락본동']
+//   };
+
+
+
+//   res.render('map/searchView', { cities, districts, neighborhoods });
+// };
+
 exports.renderSearchPage = (req, res) => {
   const cities = ['서울', '경기도', '부산광역시'];
   const districts = {
-    '서울': ['성북구', '마포구', '관악구'],
-    '경기도': ['수원시', '용인시', '성남시'],
+    '서울': ['성북구', '중구', '마포구', '관악구'],
+    '경기도': ['중구', '용인시', '성남시'],
     '부산광역시': ['해운대구', '사하구', '연제구']
   };
+  
   const neighborhoods = {
-    '성북구': ['돈암동', '길음동', '보문동'],
-    '마포구': ['합정동', '망원동', '상암동'],
-    '송파구': ['잠실2동', '잠실본동', '가락본동']
+    '서울': {
+      '성북구': ['돈암동', '길음동', '보문동'],
+      '중구': ['동대문동', '신당동', '을지로동'],
+      '마포구': ['합정동', '망원동', '상암동'],
+      '관악구': ['신림동', '서림동', '낙성대동']
+    },
+    '경기도': {
+      '중구': ['가짜동', '가짜2동', '가짜 4동'],
+    
+      '용인시': ['수지구', '처인구', '기흥구'],
+      '성남시': ['분당구', '중원구', '수정구']
+    },
+    '부산광역시': {
+      '해운대구': ['재송동', '우동', '좌동'],
+      '사하구': ['괴정동', '다대동', '하단동'],
+      '연제구': ['거제동', '연산동', '연산9동']
+    }
   };
-
-
 
   res.render('map/searchView', { cities, districts, neighborhoods });
 };
+
+
+
+
 
 exports.getSearchResults = async (req, res) => {
 
@@ -33,10 +68,7 @@ exports.getSearchResults = async (req, res) => {
     const results = await searchModel.getSearchResults(city, district, neighborhood);
     console.log("컨트롤러: "+ results[0]);
 
-    //session 에서 가져오는거
-
-    // const seller_id = req.session.seller_id;
-    // console.log("판매자 세션 아이디" + seller_id);
+  
 
     res.render('map/searchResultsView', { searchResults: results[0] });
 
